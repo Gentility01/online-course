11b09ca279531ca9e81a425978730d75b857f875
Gentility01 <mastergentility5@gmail.com>
2024-01-30 11:48:24 +0100

    updated build.sh

--- build.sh
+++ build.sh
@@ -3,3 +3,12 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py migrate
 
+
+
+
+if [[ $CREATE_SUPERUSER ]];
+then
+  python world_champ_2022/manage.py createsuperuser --no-input
+fi
+
+
